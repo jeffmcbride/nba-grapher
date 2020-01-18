f1f5ddb3b0aee246f6be8a7c1540d9d21d5be7d2
Jeff McBride <jeffmcbride@live.com>
2020-01-17 16:52:13 -0800

    Differentiate between player stats and team stats

--- app.py
+++ app.py
@@ -14,70 +14,70 @@ playerlist = players.get_players()
 clicks = 0
 
 team_stats = [
-    'TEAM_ID',
-    'TEAM_CITY',
-    'TEAM_NAME',
-    'YEAR',
-    'GP',
-    'WINS',
-    'LOSSES',
-    'WIN_PCT',
-    'CONF_RANK',
-    'DIV_RANK',
-    'PO_WINS',
-    'PO_LOSSES',
-    'CONF_COUNT',
-    'DIV_COUNT',
-    'NBA_FINALS_APPEARANCE',
-    'FGM',
-    'FGA',
-    'FG_PCT',
-    'FG3M',
-    'FG3A',
-    'FG3_PCT',
-    'FTM',
-    'FTA',
-    'FT_PCT',
-    'OREB',
-    'DREB',
-    'REB',
-    'AST',
-    'PF',
-    'STL',
-    'TOV',
-    'BLK',
-    'PTS',
-    'PTS_RANK',
+    't_TEAM_ID',
+    't_TEAM_CITY',
+    't_TEAM_NAME',
+    't_YEAR',
+    't_GP',
+    't_WINS',
+    't_LOSSES',
+    't_WIN_PCT',
+    't_CONF_RANK',
+    't_DIV_RANK',
+    't_PO_WINS',
+    't_PO_LOSSES',
+    't_CONF_COUNT',
+    't_DIV_COUNT',
+    't_NBA_FINALS_APPEARANCE',
+    't_FGM',
+    't_FGA',
+    't_FG_PCT',
+    't_FG3M',
+    't_FG3A',
+    't_FG3_PCT',
+    't_FTM',
+    't_FTA',
+    't_FT_PCT',
+    't_OREB',
+    't_DREB',
+    't_REB',
+    't_AST',
+    't_PF',
+    't_STL',
+    't_TOV',
+    't_BLK',
+    't_PTS',
+    't_PTS_RANK',
     ]
 
 player_stats = [
-    'PLAYER_ID',
-    'SEASON_ID',
-    'LEAGUE_ID',
-    'TEAM_ID',
-    'TEAM_ABBREVIATION',
-    'PLAYER_AGE',
-    'GP',
-    'GS',
-    'MIN',
-    'FGM',
-    'FGA',
-    'FG_PCT',
-    'FG3M',
-    'FG3A',
-    'FG3_PCT',
-    'FTM',
-    'FTA',
-    'FT_PCT',
-    'OREB',
-    'DREB',
-    'REB',
-    'AST',
-    'STL',
-    'BLK',
-    'TOV',
-    'PF',
-    'PTS',
+    'p_PLAYER_ID',
+    'p_SEASON_ID',
+    'p_LEAGUE_ID',
+    'p_TEAM_ID',
+    'p_TEAM_ABBREVIATION',
+    'p_PLAYER_AGE',
+    'p_GP',
+    'p_GS',
+    'p_MIN',
+    'p_FGM',
+    'p_FGA',
+    'p_FG_PCT',
+    'p_FG3M',
+    'p_FG3A',
+    'p_FG3_PCT',
+    'p_FTM',
+    'p_FTA',
+    'p_FT_PCT',
+    'p_OREB',
+    'p_DREB',
+    'p_REB',
+    'p_AST',
+    'p_STL',
+    'p_BLK',
+    'p_TOV',
+    'p_PF',
+    'p_PTS',
     ]
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -100,51 +100,59 @@ app.layout = html.Div([
     ])
 
 
-@app.callback([Output('drop1', 'options'), Output('drop2', 'options'),
-              Output('drop3', 'options'), Output('stats', 'options')],
-              [Input('teamplayer', 'value')])
+@app.callback(
+        [Output('drop1', 'options'),
+         Output('drop2', 'options'),
+         Output('drop3', 'options'),
+         Output('stats', 'options')],
+         [Input('teamplayer', 'value')])
 def setDropDown(selection):
     if selection == 'team':
-        return ([{'label': i['full_name'], 'value': i['id']} for i in
-                teamlist], [{'label': i['full_name'], 'value': i['id']}
-                for i in teamlist], [{'label': i['full_name'],
-                'value': i['id']} for i in teamlist], [{'label': i,
-                'value': i} for i in team_stats])
+        return ([{'label': i['full_name'], 'value': i['id']} for i in teamlist],
+                [{'label': i['full_name'], 'value': i['id']} for i in teamlist],
+                [{'label': i['full_name'], 'value': i['id']} for i in teamlist],
+                [{'label': i[2:], 'value': i} for i in team_stats])
     else:
 
-        return ([{'label': i['full_name'], 'value': i['id']} for i in
-                playerlist], [{'label': i['full_name'], 'value': i['id'
-                ]} for i in playerlist], [{'label': i['full_name'],
-                'value': i['id']} for i in playerlist], [{'label': i,
-                'value': i} for i in player_stats])
+        return ([{'label': i['full_name'], 'value': i['id']} for i in playerlist],
+                [{'label': i['full_name'], 'value': i['id']} for i in playerlist],
+                [{'label': i['full_name'], 'value': i['id']} for i in playerlist],
+                [{'label': i[2:], 'value': i} for i in player_stats])
 
 
-@app.callback(Output('container', 'children'), [Input('graph_btn',
-              'n_clicks'), Input('drop1', 'value'), Input('stats',
-              'values'), Input('teamplayer', 'value')])
-def graphStats(n_clicks, id, stat_list, teamplayer):
+@app.callback(
+        Output('container', 'children'),
+        [Input('graph_btn','n_clicks'),
+         Input('drop1', 'value'),
+         Input('drop2', 'value'),
+         Input('stats','values'),
+         Input('teamplayer', 'value')])
+def graphStats(n_clicks, id, id2, stat_list, teamplayer):
     global clicks
     if n_clicks == clicks or n_clicks is None:
         raise PreventUpdate
     else:
         clicks += 1
+
         if (teamplayer == 'team'):
             id1 = nba.Team(id)
+            id2 = nba.Team(id2)
         else:
             id1 = nba.Player(id)
+            id2 = nba.Player(id2)
 
-        objects = [id1]
+        objects = [id1, id2]
         graphs = []
         for j in stat_list:
             traces = []
             for i in objects:
-                df = i.get_stats(j, '1975', '2020')
+                df = i.get_stats(j[2:], '1975', '2020')
                 traces.append(dict(x=str(df.index), y=df,
                               name=i.name))
 
-                graphs.append(dcc.Graph(id=j, figure={'data': traces,
+                graphs.append(dcc.Graph(id=j[2:], figure={'data': traces,
                               'layout': dict(xaxis={'title': 'Range'},
-                              yaxis={'title': j}, title=j,
+                              yaxis={'title': j[2:]}, title=j[2:],
                               showlegend=True)}))
 
         return html.Div(graphs)
